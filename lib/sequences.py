6664e5e0eee0a52c667dd4667dc0911bdcf3e45f
Tonny Bett <tonny.bett1@student.moringaschool.com>
2025-11-24 20:11:59 +0300

    Completed sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])  # print empty list for 0 or negative
+        return
+    elif n == 1:
+        print([0])
+        return
+    
+    fib_list = [0, 1]
+    while len(fib_list) < n:
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    
+    print(fib_list)
